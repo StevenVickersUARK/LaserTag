4a9a77f102b059e51a804dd558b69a467c769e01
Ben Easterling <beneasterling25@gmail.com>
2023-03-10 18:12:39 -0600

    create image loading util

--- src/main/java/edu/uark/csce3513/team18/lasertag/splashScreen.java
+++ src/main/java/edu/uark/csce3513/team18/lasertag/splashScreen.java
@@ -10,6 +10,7 @@ import javax.swing.*;
 public class splashScreen extends JFrame
 {
     private final int duration = 3000; // show splash screen for 3 seconds
+    private static final String LOGO_NAME = "logo.jpg";
 
     public splashScreen()
     {
@@ -17,28 +18,21 @@ public class splashScreen extends JFrame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.getContentPane().setBackground(Color.black);
 
-        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-        InputStream is = classloader.getResourceAsStream("logo.jpg");
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        int nRead;
-        byte[] data = new byte[16384];
-
         try {
-            while ((nRead = is.read(data, 0, data.length)) != -1) {
-                buffer.write(data, 0, nRead);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+            ImageIcon logo = ImageUtil.loadImageIcon(LOGO_NAME);
+        
+            Image tempImage1 = logo.getImage();
+            Image tempImage2 = tempImage1.getScaledInstance(1280, 720, Image.SCALE_SMOOTH); // resizing image
+            logo = new ImageIcon(tempImage2);
+    
+            JLabel label = new JLabel(logo);
+            this.getContentPane().add(label);
+            Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+            setLocation((size.width - getWidth()) / 2, (size.height - getHeight()) / 2); // place image in center
+        } catch (ImageException ex) {
+            System.err.println("Unable to locate logo image!");
+            ex.printStackTrace();
         }
-        ImageIcon logo = new ImageIcon(buffer.toByteArray()); // path needs to be changed before file is merged
-        Image tempImage1 = logo.getImage();
-        Image tempImage2 = tempImage1.getScaledInstance(1280, 720, Image.SCALE_SMOOTH); // resizing image
-        logo = new ImageIcon(tempImage2);
-
-        JLabel label = new JLabel(logo);
-        this.getContentPane().add(label);
-        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
-        setLocation((size.width - getWidth()) / 2, (size.height - getHeight()) / 2); // place image in center
     }
 
     public void showSplashScreen()
