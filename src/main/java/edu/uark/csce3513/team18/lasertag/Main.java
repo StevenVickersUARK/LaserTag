f79681f9550c199ce446c492fcd65c7f0c9442f7
Ben Easterling <beneasterling25@gmail.com>
2023-03-10 18:47:48 -0600

    add code formatting settings to format code on save

--- src/main/java/edu/uark/csce3513/team18/lasertag/Main.java
+++ src/main/java/edu/uark/csce3513/team18/lasertag/Main.java
@@ -2,29 +2,29 @@ package edu.uark.csce3513.team18.lasertag;
 
 import java.sql.SQLException;
 import java.util.List;
+
 public class Main {
-    public static void main(String[] args) throws SQLException 
-    {
-        String url = "jdbc:postgresql://db.xbbsojmzbwlxprdwscxj.supabase.co:5432/postgres";
-		String username = "postgres";
-		
-		String password = "team18isthebest";
-		Database database = new Database(url, username, password);
-		try {
-			database.connect();
-			List<Player> players = database.getPlayers();
-			System.out.println("Players:");
-			for (Player player : players) {
-				player.print();
-			}
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
+  public static void main(String[] args) throws SQLException {
+    String url = "jdbc:postgresql://db.xbbsojmzbwlxprdwscxj.supabase.co:5432/postgres";
+    String username = "postgres";
 
-		new TeamIntroScreen();
-		new splashScreen().showSplashScreen();
-
-        Entry entry = new Entry();
-        entry.showEntryScreen();
+    String password = "team18isthebest";
+    Database database = new Database(url, username, password);
+    try {
+      database.connect();
+      List<Player> players = database.getPlayers();
+      System.out.println("Players:");
+      for (Player player : players) {
+        player.print();
+      }
+    } catch (SQLException ex) {
+      ex.printStackTrace();
     }
+
+    new TeamIntroScreen();
+    new splashScreen().showSplashScreen();
+
+    Entry entry = new Entry();
+    entry.showEntryScreen();
+  }
 }
