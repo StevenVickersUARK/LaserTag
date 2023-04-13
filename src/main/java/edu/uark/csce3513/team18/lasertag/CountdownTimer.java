9b3a8d6f4c2e628915191a4ca4c29de854f36798
Ben Easterling <beneasterling25@gmail.com>
2023-04-13 15:59:50 -0500

    reformat

--- src/main/java/edu/uark/csce3513/team18/lasertag/CountdownTimer.java
+++ src/main/java/edu/uark/csce3513/team18/lasertag/CountdownTimer.java
@@ -3,52 +3,41 @@ package edu.uark.csce3513.team18.lasertag;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class CountdownTimer
-{
-  int count = 0;
-  Timer timer;
-  boolean isRunning;
+public class CountdownTimer {
+    int count = 0;
+    Timer timer;
+    boolean isRunning;
 
-  public CountdownTimer(int count)
-  {
-    this.count = count;
-    this.isRunning = false;
-  }
+    public CountdownTimer(int count) {
+        this.count = count;
+        this.isRunning = false;
+    }
 
-  public void start()
-  {
-    if(!isRunning)
-    {
-      timer = new Timer();
-      timer.scheduleAtFixedRate(new TimerTask() 
-      { 
-        public void run() 
-        {
-          if (count > 0) {
-            count--;
-          System.out.println(count);
-          } 
-          else 
-          {
-            stop();
-          }
+    public void start() {
+        if (!isRunning) {
+            timer = new Timer();
+            timer.scheduleAtFixedRate(new TimerTask() {
+                public void run() {
+                    if (count > 0) {
+                        count--;
+                        System.out.println(count);
+                    } else {
+                        stop();
+                    }
+                }
+            }, 0, 1000);
+            isRunning = true;
         }
-      }, 0, 1000);
-      isRunning = true;
     }
-  }
 
-  public void stop()
-  {
-    if(isRunning)
-    {
-      timer.cancel();
-      isRunning = false;
+    public void stop() {
+        if (isRunning) {
+            timer.cancel();
+            isRunning = false;
+        }
     }
-  }
 
-  public int getCount()
-  {
-    return count;
-  }
+    public int getCount() {
+        return count;
+    }
 }
